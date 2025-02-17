488d576c7dd8109366abdd1a077375a92342bd6d
mandalvikash <146217029+mandalvikash@users.noreply.github.com>
2025-02-17 17:36:59 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ const app = express();
 
 
 app.use(cors({
-  origin:["https://tax-caluclation-72ne.vercel.app","http://localhost:5000"]}));
+  origin:["https://tax-caluclation-72ne-7mqyk662b-vikash-mandals-projects.vercel.app","http://localhost:5000"]}));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
